9ed6bdcce6b8c3ee75566486b6e86573c5a4a621
한재원 <hanjo8813@gmail.com>
2021-06-05 23:47:21 +0900

    .

--- src/views/MeetingRoom.js
+++ src/views/MeetingRoom.js
@@ -40,7 +40,7 @@ class MeetingRoom extends Component {
     // ------------------------------------------------------ Web RTC 요소 핸들링 ------------------------------------------------------
 
     var connection = new window.RTCMultiConnection();
-    connection.socketURL = 'http://rtcmulticonnection.herokuapp.com:443/';
+    connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
 
     connection.session = {
       audio: true,
